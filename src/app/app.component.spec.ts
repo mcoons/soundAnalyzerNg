05c3b5e44c0f85018395b946b1a44decb755a7de
mcoons <mcoons67@gmail.com>
2020-04-29 20:05:57 -0600

    renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'soundamalyzerNg'`, () => {
+  it(`should have as title 'soundanalyzerng'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('soundamalyzerNg');
+    expect(app.title).toEqual('soundanalyzerng');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to soundamalyzerNg!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to soundanalyzerng!');
   });
 });
